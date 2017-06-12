a92561b9f8dddb8e0e592b297258f351f5373b9b
Mohan Chandra <mohan.chandra@oracle.com>
2017-06-12 13:48:41 +0530

    Removing the styles for merge conflicts

--- src/js/config/services.js
+++ src/js/config/services.js
@@ -14,20 +14,20 @@ define(['knockout', 'config/serviceConfig'
 
         var self = this;
         
-        // Context root for dev
         var ctx = '';
         if (location.origin.indexOf('localhost') > 0) {
             if (location.protocol === 'http:') {
                 self.portalRestHost = ko.observable("http://localhost:8080".concat(ctx));
             } 
         } else {
-            // For context root to be relative on PROD
             self.portalRestHost = ko.observable("https://docsportalservices-inoracless38727.apaas.us2.oraclecloud.com".concat(ctx));
         }
+        
         self.login = function(payload) {
             var serviceURL = self.portalRestHost() + "/login";
-            return serviceConfig.authenticatePost(serviceURL, payload, serviceConfig.contentFormUrlEncoded);
-        }
+            return serviceConfig.authenticatePost(serviceURL, payload, serviceConfig.contentTypeFormUrlEncoded);
+        };
+        
         self.uploadDocument = function (document, invoiceNumber) {
             var serviceURL = self.portalRestHost() + "/docs/upload/uploadFile?invoiceNumber=" + invoiceNumber;
             return serviceConfig.callPostService(serviceURL, document, serviceConfig.contentTypeMultipartFormData);
